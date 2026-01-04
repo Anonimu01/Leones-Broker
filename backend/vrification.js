76b91df29cca8b5ed47561bcdbf3acbbe649ea96
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-04 13:11:22 -0800

    Create vrification.js

---
+++ backend/vrification.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const verificationSchema = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId,
+  documentUrl: String,
+  addressProofUrl: String,
+  status: { type: String, default: 'pending' }
+},{ timestamps:true });
+
+export default mongoose.model('Verification', verificationSchema);
