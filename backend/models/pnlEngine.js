f904ce65dddf50c119c3c46c5b69556be12bbbc1
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-28 12:11:00 -0800

    Delete backend directory

--- backend/models/pnlEngine.js
+++
@@ -1,4 +0,0 @@
-function calculatePnL(entry, price, qty, leverage){
-  return ((price - entry) * qty) * leverage;
-}
-module.exports = { calculatePnL };
--- backend/models/routes kyc.js
+++
@@ -1,8 +0,0 @@
-router.post('/upload', auth, async(req,res)=>{
-  await Kyc.create({
-    userId: req.user.id,
-    documentId: req.body.doc,
-    proofAddress: req.body.address
-  });
-  res.json({msg:'KYC enviado'});
-});
