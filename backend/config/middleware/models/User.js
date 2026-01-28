5a334a0c31b2dcd1a2967b59fb069bd70045424e
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-28 12:28:05 -0800

    Create User.js

---
+++ backend/config/middleware/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    balance: { type: Number, default: 0 },
+    leverage: { type: Number, default: 1 },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    verified: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
