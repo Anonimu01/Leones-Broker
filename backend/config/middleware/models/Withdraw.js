6d611f41d412980c32067b92dff278db8d563e6a
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-28 12:29:49 -0800

    Create Withdraw.js

---
+++ backend/config/middleware/models/Withdraw.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const withdrawSchema = new mongoose.Schema(
+  {
+    userId: mongoose.Schema.Types.ObjectId,
+    amount: Number,
+    status: { type: String, default: "pending" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Withdraw", withdrawSchema);
