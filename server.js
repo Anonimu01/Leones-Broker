48efffd3ad9237fef6797065387ba0197ec67e0c
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-29 09:42:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import { connectDB } from "./config/db.js";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import verificationRoutes from "./routes/verification.routes.js";
@@ -20,10 +21,12 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
+// API
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/verify", verificationRoutes);
 
+// FRONTEND
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
@@ -31,6 +34,7 @@ app.get("*", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log("🚀 Cliente activo en puerto", PORT)
-);
+
+app.listen(PORT, () => {
+  console.log("🚀 Cliente activo en puerto", PORT);
+});
