fb480ccdd76ead001bf53a4bae1bc55278fd7482
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-29 09:47:20 -0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: String,
+
   email: {
     type: String,
     unique: true
   },
+
   password: String,
 
   verified: {
