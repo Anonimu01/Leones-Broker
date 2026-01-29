468f20c2f5478113328c37b0b2d4f02d08b550c7
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-29 08:13:39 -0800

    Create verification.routes.js

---
+++ routes/verification.routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+router.get("/email/:token", async (req, res) => {
+  const user = await User.findOne({
+    verificationToken: req.params.token
+  });
+
+  if (!user)
+    return res.send("Token inválido");
+
+  user.verified = true;
+  user.verificationToken = null;
+  await user.save();
+
+  res.send(`
+    <h2>✅ Correo verificado correctamente</h2>
+    <p>Ya puedes iniciar sesión.</p>
+  `);
+});
+
+export default router;
