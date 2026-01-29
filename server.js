676b8be8b7c5e0ded9fe0b84bafb337a7222e10a
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-01-29 07:55:32 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import verificationRoutes from "./routes/verification.routes.js";
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/verify", verificationRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log("🚀 Cliente activo en puerto", PORT)
+);
