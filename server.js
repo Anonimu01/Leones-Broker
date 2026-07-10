92c0cffa4c480ff8904d74e927c28406e0b672e8
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-07-10 10:24:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,6 @@ const allowedOrigins = new Set(
     "http://127.0.0.1:3000",
     "http://localhost:4000",
     "http://127.0.0.1:4000",
-    "https://leones-broker.onrender.com",
     "https://leones-broker-production.up.railway.app",
     "https://leonesbr.pro",
   ].filter(Boolean)
